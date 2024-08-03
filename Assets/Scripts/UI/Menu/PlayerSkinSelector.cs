8264f3116fb1c0fe76ff043aa67d3b61cfbc9a1e
LEGO <matias.labreniuk@davinci.edu.ar>
2024-08-03 18:16:27 -0300

    Menu
    Player skin selector casi terminado

---
+++ Assets/Scripts/UI/Menu/PlayerSkinSelector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerSkinSelector : MonoBehaviour
+{
+    [SerializeField] GameObject _blocked, _bonusAttackStat, _bonusMiningStat;
+    [SerializeField] TextMeshProUGUI _fragments;
+    [SerializeField] Color _blockedColor, _activeColor, _equipableColor;
+    [SerializeField] Animator _skinViewerAnimator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
